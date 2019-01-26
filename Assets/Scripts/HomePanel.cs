fe1723a10b003247b764fe4caa906725f53df489
franckentien <franckentien@hotmail.fr>
2019-01-26 08:01:32 +0100

    Create Backhome Button

---
+++ Assets/Scripts/HomePanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The home panel.
+/// </summary>
+public class HomePanel : MonoBehaviour
+{
+    public ValuesPanel valuesPanel;
+
+
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start () {
+		
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update () {
+		
+	}
+
+    /// <summary>
+    /// The back to home button click.
+    /// </summary>
+    public void backToHomeButtonClick()
+    {
+        this.valuesPanel.UpdateEnergie(5);
+    }
+}
